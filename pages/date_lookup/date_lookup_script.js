31c76d3210f7d9410fe6952e222ff177b9bb3863
Owen Chen <owenchenyp@gmail.com>
2024-02-07 09:19:31 -0500

    worked on dl

---
+++ pages/date_lookup/date_lookup_script.js
@@ -0,0 +1,33 @@
+function search() {
+    // Retrieve values from the inputs
+    var month = document.getElementById('month_select').value;
+    var day = document.getElementById('day_select').value;
+    var period = document.getElementById('period_select').value;
+
+    // Do something with the values
+    console.log("Month: " + month);
+    console.log("Day: " + day);
+    console.log("Period: " + period);
+    
+    // You can further process or store these values as needed
+}
+
+function reserve() {
+    console.log("TEST");
+    
+    var id = prompt("Enter ID:");
+    var location = prompt("Enter Location:");
+    var numOfBins = prompt("Enter Number of Bins Available:");
+    
+    console.log("id: " + id);
+    console.log("location: " + location);
+    console.log("numOfBins: " + numOfBins);
+    // Do something with the inputs (e.g., validate, process)
+    if (id && location && numOfBins) {
+        alert("Reservation successful!");
+    } else {
+        alert("Please fill in all fields.");
+    }
+
+}
+    
