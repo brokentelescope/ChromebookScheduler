46b6d01eebf4ded27c97ea0d3f877715aa19e081
Owen Chen <owenchenyp@gmail.com>
2024-02-23 10:07:28 -0500

    added flasksqlalchemy

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
-
+# from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# db = SQLAlchemy(app)
 
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.column(db.String(200), nullable=False)
+#     completed = db.column(db.Integer, default=0)
+#     date_created = db.Column(db.DateTime, default=datetime.estnow)
+
+#     def __repr__(self):
+#         return '<Task %r>' % self.id
+    
 @app.route('/')
-def index():
+def home_index():
     return render_template('home_index.html')
 
 @app.route('/date_lookup')
