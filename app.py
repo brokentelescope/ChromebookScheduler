2aed3921e39acb548b5c4ae9a38054aa96f2c3b8
Owen Chen <owenchenyp@gmail.com>
2024-02-23 09:42:10 -0500

    bruh

--- app.py
+++ app.py
@@ -6,5 +6,22 @@ app = Flask(__name__)
 def index():
     return render_template('home_index.html')
 
+@app.route('/date_lookup')
+def date_lookup():
+    return render_template('date_lookup_index.html')
+
+@app.route('/my_reservations')
+def my_reservations():
+    return render_template('my_reservations_index.html')
+
+@app.route('/reservation_history')
+def reservation_history():
+    return render_template('reservation_history_index.html')
+
+# @app.route('/bug_report')
+# def bug_report():
+#     return render_template('bug_report.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
