b3cf240c2a712eabf1f3c4c4aa36db7715b81b14
Owen Chen <owenchenyp@gmail.com>
2024-02-27 08:41:39 -0500

    commit

--- app.py
+++ app.py
@@ -1,22 +1,38 @@
-from flask import Flask, render_template
-# from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import sqlite3
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 # class Todo(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
-#     content = db.column(db.String(200), nullable=False)
-#     completed = db.column(db.Integer, default=0)
+#     content = db.column(db.String(200))
+#     completed = db.column(db.Integer)
 #     date_created = db.Column(db.DateTime, default=datetime.estnow)
 
 #     def __repr__(self):
 #         return '<Task %r>' % self.id
     
-@app.route('/')
-def home_index():
-    return render_template('home_index.html')
+@app.route('/', methods=['GET', 'POST'])
+def login_index():
+    if request.method == 'POST':
+        # sqlite
+        connection = sqlite3.connect('user_data.db')
+        cursor = connection.cursor()
+        # html form 
+    return render_template('login_index.html')
+    
+
+
+
+    # return render_template('home_index.html')
+
+
+# @app.route('/')
+# def home_index():
+#     return render_template('home_index.html')
 
 @app.route('/date_lookup')
 def date_lookup():
