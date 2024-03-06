655656ea1a0dc9035292582895396cdf0240c8e4
Owen Chen <owenchenyp@gmail.com>
2024-03-06 10:11:35 -0500

    updated datelookup js/html

--- static/date_lookup_script.js
+++ static/date_lookup_script.js
@@ -1,25 +1,25 @@
 // your code here
-function search() {
-    // Retrieve values from the inputs
-    var month = document.getElementById('month_select').value;
-    var day = document.getElementById('day_select').value;
-    var period = document.getElementById('period_select').value;
 
-    // Do something with the values
-    console.log("Month: " + month);
-    console.log("Day: " + day);
-    console.log("Period: " + period);
-    
-    // You can further process or store these values as needed
-}
+
+// function to search and display bins
+function search() {
+    var date = document.getElementById("dateInput").value;
+    var sel = document.getElementById("periodInput");
+    var period = sel.options[sel.selectedIndex].text
+    console.log(date, period);
+
+
+   
+}      
+
 
 function reserve() {
     console.log("TEST");
-    
+   
     var id = prompt("Enter ID:");
     var location = prompt("Enter Location:");
     var numOfBins = prompt("Enter Number of Bins Available:");
-    
+   
     console.log("id: " + id);
     console.log("location: " + location);
     console.log("numOfBins: " + numOfBins);
@@ -30,16 +30,20 @@ function reserve() {
         alert("Please fill in all fields.");
     }
 
+
 }
+
 
 document.addEventListener("DOMContentLoaded", function(event) { // Reference Tracker 2
     // Reference Tracker 1
     // code to auto set the default values for the date input
     var today = new Date().toISOString().slice(0, 10);
-    console.log(document.getElementById("dateInput"));
     var date = document.getElementById("dateInput");
     date.value = today;
     date.min = today;
 
-    console.log(today);
+
 });
+
+
+
